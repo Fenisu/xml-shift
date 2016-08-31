acb2371a55972b3b64bac5f93e5f9f6b0dd11a74
Ignacio Quezada <dreamtrick@gmail.com>
2016-08-31 23:55:57 +0200

    Simple working version, modifiesxml files in the same directory

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import xml.etree.ElementTree as ET
+import glob
+import sys
+
+SHIFT = 160
+
+
+def shift_root_xml(root):
+    for graphic in root.iter('Graphic'):
+        graphic.set('X', '{}'.format(int(graphic.get('X')) - SHIFT))
+
+
+def main():
+    for file in glob.glob('*.xml'):
+        try:
+            tree = ET.parse(file)
+            root = tree.getroot()
+            shift_root_xml(root)
+            tree.write('new-{}'.format(file))
+        except:
+            print("Script failed!!")
+            sys.exit(2)
+
+
+if __name__ == '__main__':
+    main()
